2d947ac0842a62f101b9f6837cebbf66e9b35e19
BanjoSledge <martin.kelly4000@gmail.com>
2019-04-24 17:56:58 +1000

    Updated

--- setupSingleProject.sh
+++ setupSingleProject.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/TheSledgeHammer/RetroBees.git
 git clone https://github.com/TheSledgeHammer/GroovyMC.git
-git clone https://github.com/TheSledgeHammer/SingleProject.git
+git clone https://github.com/TheSledgeHammer/GroovyForge.git
+#git clone https://github.com/TheSledgeHammer/SingleProject.git
